71e12fd894c433650ed0b14fd8e3a434dd74fece
taetae <wendy1080@naver.com>
2024-03-19 15:57:32 +0900

    fix calculate iou

--- src/calculate_iou.py
+++ src/calculate_iou.py
@@ -6,32 +6,43 @@ import numpy as np
 
 # 1안의 iou값 계산 근데 보면 동일한 위치 선상에서 비교하는게 아님
 # def calculate_IOU(image_path1, image_path2):
-#     # 이미지 읽어오기 및 grayscale 변환
-#     image1 = cv2.imread(image_path1)
-#     image2 = cv2.imread(image_path2)
-#     image_1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-#     image_2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-#
+#     image1 = cv2.imread(image_path1, cv2.IMREAD_GRAYSCALE)
+#     image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
 #     # 이미지 이진화
-#     res, thresh1 = cv2.threshold(image_1, 195, 255, cv2.THRESH_BINARY)
-#     res, thresh2 = cv2.threshold(image_2, 195, 255, cv2.THRESH_BINARY)
+#     _, thresh1 = cv2.threshold(image1, 230, 255, cv2.THRESH_BINARY)
+#     _, thresh2 = cv2.threshold(image2, 230, 255, cv2.THRESH_BINARY)
 #
+#     # 컨투어 찾기
+#     contours1, _ = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     contours2, _ = cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 #
-#     # IOU를 위한 intersection , union구하기 => 수정 필요 동일한 위치 선상에서 비교할 수있도록 코드 수정해야할듯
-#     intersection = cv2.countNonZero(cv2.bitwise_and(thresh1, thresh2))
-#     union = cv2.countNonZero(cv2.bitwise_or(thresh1,thresh2))
+#     # 두 이미지의 컨투어 영역 계산
+#     area1 = cv2.contourArea(contours1[0]) if contours1 else 0
+#     area2 = cv2.contourArea(contours2[0]) if contours2 else 0
 #
-#     IOU = intersection / union
-#     return IOU
+#     # 겹치는 영역 계산
+#     intersection_area = 0
+#     if area1 and area2:
+#         intersection = cv2.bitwise_and(thresh1, thresh2)
+#         intersection_contours, _ = cv2.findContours(intersection, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#         intersection_area = cv2.contourArea(intersection_contours[0]) if intersection_contours else 0
+#
+#     # IOU 계산
+#     union_area = area1 + area2 - intersection_area
+#     iou = intersection_area / union_area if union_area != 0 else 0
+#
+#     return iou
 
 # 2안의 iou값 계산
 def calculate_IOU(image_path1, image_path2):
 
     image1 = cv2.imread(image_path1, cv2.IMREAD_GRAYSCALE)
     image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
+    # cv2.imshow('image1', image1)
+    # cv2.imshow('image2', image2)
 
-    _, thresh1 = cv2.threshold(image1, 180, 255, cv2.THRESH_BINARY)
-    _, thresh2 = cv2.threshold(image2, 180, 255, cv2.THRESH_BINARY)
+    _, thresh1 = cv2.threshold(image1, 240, 255, cv2.THRESH_BINARY)
+    _, thresh2 = cv2.threshold(image2, 240, 255, cv2.THRESH_BINARY)
 
     # 두 이미지의 모든 픽셀을 비교하여 겹치는 영역 계산
     # numpyarray에서 임계값을 기반으로 이진화된 이미지에서, 동일한 이미지에서 두 픽셀의 결과를 비교하여 겹치는 부분 저장
@@ -39,18 +50,27 @@ def calculate_IOU(image_path1, image_path2):
 
     intersection = np.logical_and(thresh1, thresh2)
     intersection_area = np.sum(intersection)
-    # print(intersection_area)
 
-    # 두 이미지의 흰색(전체 영역) 계산
-    area1 = np.sum(thresh1)
-    area2 = np.sum(thresh2)
+    # # 시각화 용
+    # intersection_image = np.zeros_like(image1)
+    # intersection_image[intersection] = 255
+    # cv2.imshow("thresh1", thresh1)
+    # cv2.imshow("thresh2", thresh2)
+    # cv2.imshow("intersection", intersection_image)
+    # cv2.waitKey(0)
 
-    # IOU 계산
-    union_area = area1 + area2 - intersection_area
-    iou = intersection_area / union_area if union_area != 0 else 0
+    # union
+    union = np.logical_or(thresh1, thresh2)
+    union_area = np.sum(union)
+
+    union_image  = np.zeros_like(image1)
+    union_image[union] = 255
+    # cv2.imshow("union_image", union_image)
+    # cv2.waitKey(0)
+
+    iou = intersection_area / union_area
 
     return iou
-
 
 # file이름 에서 track_id별로 비교하기 위해서 숫자 뽑아내기
 def extract_number_from_filename(filename):
@@ -78,7 +98,8 @@ def main(folder_path):
                 for j in range(i + 1, len(images)):
                     image_path1 = os.path.join(folder_path, images[i])
                     image_path2 = os.path.join(folder_path, images[j])
-                    # print(image_path1,"/",image_path2)
+                    print(image_path1,"/",image_path2)
+
 
                     iou = calculate_IOU(image_path1, image_path2)
                     print(iou)
--- src/ddd.py
+++ src/ddd.py
@@ -67,32 +67,36 @@
 import cv2
 import numpy as np
 
-def calculate_IOU(image1, image2):
-    # 이미지 이진화
-    _, thresh1 = cv2.threshold(image1, 180, 255, cv2.THRESH_BINARY)
-    _, thresh2 = cv2.threshold(image2, 180, 255, cv2.THRESH_BINARY)
+def calculate_IOU(image_path1, image_path2):
 
-    # 두 이미지의 모든 픽셀을 비교하여 겹치는 영역 계산
+    image1 = cv2.imread(image_path1, cv2.IMREAD_GRAYSCALE)
+    image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
+    _, thresh1 = cv2.threshold(image1, 240, 255, cv2.THRESH_BINARY)
+    _, thresh2 = cv2.threshold(image2, 240, 255, cv2.THRESH_BINARY)
+
+
     intersection = np.logical_and(thresh1, thresh2)
     intersection_area = np.sum(intersection)
     print(intersection_area)
 
-    # 두 이미지의 흰색(전체 영역) 계산
-    area1 = np.sum(thresh1)
-    area2 = np.sum(thresh2)
 
-    # IOU 계산
-    union_area = area1 + area2 - intersection_area
-    iou = intersection_area / union_area if union_area != 0 else 0
+    # union
+    union = np.logical_or(thresh1, thresh2)
+    union_area = np.sum(union)
+    print(union_area)
+
+    union_image  = np.zeros_like(image1)
+    union_image[union] = 255
+    cv2.imshow("union_image", union_image)
+    cv2.waitKey(0)
+
+    iou = intersection_area / union_area
 
     return iou
 
 if __name__ == "__main__":
-    image_path1 = "output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/tmp/visualize/density_map/update1_tmp_10_1_sum.png"  # 첫 번째 이미지 경로
-    image_path2 = "output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/tmp/visualize/density_map/update1_tmp_10_2_sum.png"  # 두 번째 이미지 경로
+    image_path1 = "output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/tmp/visualize/density_map/update1_tmp_10_4_sum.png"  # 첫 번째 이미지 경로
+    image_path2 = "output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/tmp/visualize/density_map/update1_tmp_10_5_sum.png"  # 두 번째 이미지 경로
 
-    image1 = cv2.imread(image_path1, cv2.IMREAD_GRAYSCALE)
-    image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
-
-    iou = calculate_IOU(image1, image2)
+    iou = calculate_IOU(image_path1, image_path2)
     print("IOU:", iou)
