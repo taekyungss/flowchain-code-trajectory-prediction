e400f3c66f4e7631f45c62e4503f7d9d6631e351
taetae <wendy1080@naver.com>
2024-06-03 10:54:03 +0900

    Chore: spark_pixel code

--- src/risk_calculate.py
+++ src/risk_calculate.py
@@ -5,25 +5,21 @@ import torch.nn.functional as F
 import torch
 
 
+# 저장된 density map에서 픽셀이 튀는 부분(가장 진한 부분)을 찾고 그것 끼리의 거리를 측정하는 코드
+
 def spark_pixel(path):
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     pixels = gray.flatten()
     min_intensity_pixel = np.unravel_index(np.argmin(pixels), gray.shape)
 
-    # 시각화 코드
-    # image_with_min_loc = cv2.circle(image.copy(), (min_intensity_pixel[1], min_intensity_pixel[0]), 5, (0, 0, 255),
-    #                                 2)  # BGR 컬러로 표시하므로 (0, 0, 255)는 빨간색을 의미합니다.
-    # plt.imshow(image_with_min_loc)
-    # plt.show()
 
     min_intensity_pixel = torch.Tensor(list(min_intensity_pixel))
     return min_intensity_pixel
 
-a = "112_11"
-b = "112_12"
-path1 = f"output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/eth/density_map/update1_biwi_eth_{a}_sum.png"
-path2 = f'output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/eth/density_map/update1_biwi_eth_{b}_sum.png'
+# 여기다가 각각 iamge pair 입력
+path1 = f"output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/eth/density_map/update1_biwi_eth_112_11_sum.png"
+path2 = f'output/config/TP/FlowChain/CIF_separate_cond_v_trajectron/eth/density_map/update1_biwi_eth_112_12_sum.png'
 
 distance = float(F.pairwise_distance(spark_pixel(path1), spark_pixel(path2)))
 print(distance)
